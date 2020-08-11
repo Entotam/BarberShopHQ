adee09a9063e76f3ddcfbe6118267d6708ce60c3
Sergei Chernov <dozzator@gmail.com>
2020-08-11 07:07:30 +0400

    add valitation and keep values on post

--- app.rb
+++ app.rb
@@ -7,6 +7,10 @@ require 'sinatra/activerecord'
 set :database, { adapter: 'sqlite3', database: 'barbershop.db' }
 
 class Client < ActiveRecord::Base
+	validates :name, presence: true
+	validates :phone, presence: true
+	validates :datestamp, presence: true
+	validates :color, presence: true
 end
 
 class Barber < ActiveRecord::Base
@@ -25,14 +29,18 @@ get '/' do
 end
 
 get '/visit' do
-
+	@c = Client.new
 	erb :visit
 end
 
 post '/visit' do
 
-	c = Client.new params[:client]
-	c.save
+	@c = Client.new params[:client]
+	if @c.save
+		erb "Вы записались!"
+	else
+		@error = @c.errors.full_messages.first
+	end
 
 	erb :visit
 end
