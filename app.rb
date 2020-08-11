b368aead4387691f766aeded23c09f6655fef6bb
Sergei Chernov <dozzator@gmail.com>
2020-08-11 08:09:04 +0400

    add barber page

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ require 'sinatra/activerecord'
 set :database, { adapter: 'sqlite3', database: 'barbershop.db' }
 
 class Client < ActiveRecord::Base
-	validates :name, presence: true
+	validates :name, presence: true, length: { minimum: 3 }
 	validates :phone, presence: true
 	validates :datestamp, presence: true
 	validates :color, presence: true
@@ -59,4 +59,9 @@ post '/contacts' do
 	contact.save
 
 	erb :contacts
+end
+
+get '/barber/:id' do
+	@barber = Barber.find(params[:id])
+	erb :barber
 end
