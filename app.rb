99d3693b9fd531a98b051f7ae226496c1e439a31
Sergei Chernov <dozzator@gmail.com>
2020-08-11 06:33:36 +0400

    New way to save input to db

--- app.rb
+++ app.rb
@@ -12,6 +12,43 @@ end
 class Barber < ActiveRecord::Base
 end
 
+class Contact <ActiveRecord::Base
+end
+
+before do
+	@barbers = Barber.all
+end
+
 get '/' do
+	
 	erb :index
+end
+
+get '/visit' do
+
+	erb :visit
+end
+
+post '/visit' do
+
+	c = Client.new params[:client]
+	c.save
+
+	erb :visit
+end
+
+get '/contacts' do
+	erb :contacts
+end
+
+post '/contacts' do
+	@email = params[:email]
+	@message = params[:message_contacts]
+
+	contact = Contact.new
+		contact.email = @email
+		contact.message = @message
+	contact.save
+
+	erb :contacts
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_04_012419) do
+ActiveRecord::Schema.define(version: 2020_08_08_023351) do
 
   create_table "barbers", force: :cascade do |t|
     t.text "name"
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 2020_08_04_012419) do
     t.text "datestamp"
     t.text "barber"
     t.text "color"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.text "email"
+    t.text "message"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
